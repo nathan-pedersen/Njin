44d376e6d94f47d02100f5b1ea2d40f7068bda44
Nathan Pedersen <ntpedersen1@gmail.com>
2025-08-10 23:01:55 -0800

    Templated Vec2, added rotate function

--- src/Vec2.cpp
+++ src/Vec2.cpp
@@ -1,88 +1,117 @@
 #include "Vec2.h"
 
-bool Vec2::operator==(const Vec2& rhs) const
+template <typename T>
+bool Vec2<T>::operator==(const Vec2<T>& rhs) const
 {
     if (x == rhs.x && y == rhs.y)
         return true;
     return false;
 }
 
-Vec2 Vec2::operator+(const Vec2& rhs) const
+template <typename T>
+Vec2<T> Vec2<T>::operator+(const Vec2<T>& rhs) const
 {
     return {x + rhs.x, y + rhs.y};
 }
 
-Vec2 Vec2::operator-(const Vec2& rhs) const
+template <typename T>
+Vec2<T> Vec2<T>::operator-(const Vec2<T>& rhs) const
 {
     return {x - rhs.x, y - rhs.y};
 }
 
-Vec2 Vec2::operator*(const f32& scalar) const
+template <typename T>
+Vec2<T> Vec2<T>::operator*(const T& scalar) const
 {
     return {x * scalar, y * scalar};
 }
 
-Vec2 Vec2::operator/(const Vec2& rhs) const
+template <typename T>
+Vec2<T> Vec2<T>::operator/(const Vec2<T>& rhs) const
 {
     return {x / rhs.x, y / rhs.y};
 }
 
-void Vec2::operator+=(const Vec2& rhs)
+template <typename T>
+void Vec2<T>::operator+=(const Vec2<T>& rhs)
 {
     x += rhs.x;
     y += rhs.y;
 }
 
-void Vec2::operator-=(const Vec2& rhs)
+template <typename T>
+void Vec2<T>::operator-=(const Vec2<T>& rhs)
 {
     x -= rhs.x;
     y -= rhs.y;
 }
 
-void Vec2::operator*=(const Vec2& rhs)
+template <typename T>
+void Vec2<T>::operator*=(const Vec2<T>& rhs)
 {
     x *= rhs.x;
     y *= rhs.y;
 }
 
-void Vec2::operator/=(const Vec2& rhs)
+template <typename T>
+void Vec2<T>::operator/=(const Vec2<T>& rhs)
 {
     x /= rhs.x;
     y /= rhs.y;
 }
 
-Vec2::operator sf::Vector2f()
+template <typename T>
+Vec2<T>::operator sf::Vector2f()
 {
     return sf::Vector2f(x, y);
 }
 
-f32 Vec2::length()
+template <typename T>
+f32 Vec2<T>::length()
 {
     return std::sqrt(x * x + y * y);
 }
 
-void Vec2::normalize()
+template <typename T>
+void Vec2<T>::normalize()
 {
     f32 l = this->length();
     x     = x / l;
     y     = y / l;
 }
 
-Vec2 Vec2::normalized() const
+template <typename T>
+Vec2<T> Vec2<T>::normalized() const
 {
-    Vec2 tmp = *this;
+    Vec2<T> tmp = *this;
     tmp.normalize();
     return tmp;
 }
 
-Vec2 Vec2::randomPointInBounds(const Vec2& topLeft, const Vec2& bottomRight)
+template <typename T>
+Vec2<T> Vec2<T>::randomPointInBounds(const Vec2<T>& topLeft, const Vec2<T>& bottomRight)
 {
     i32 x = rand_in_range_i32(topLeft.x, bottomRight.x);
     i32 y = rand_in_range_i32(topLeft.y, bottomRight.y);
-    return Vec2(x, y);
+    return Vec2<T>(x, y);
 }
 
-void Vec2::print() const
+Vec2<f32> Vec2<f32>::rotate(f32 degrees) const
+{
+    f32 radians = degrees * (M_PI / 180.0f);
+    f32 cosDeg  = cosf(radians);
+    f32 sinDeg  = sinf(radians);
+    f32 bx      = (cosDeg * this->x) + (-sinDeg * this->y);
+    f32 by      = (sinDeg * this->x) + (cosDeg * this->y);
+    return Vec2<f32>{bx, by};
+}
+
+template <typename T>
+void Vec2<T>::print() const
 {
     std::println("[{}, {}]", x, y);
 }
+
+// Explicit instantiations for the types you use, e.g. float, int
+template class Vec2<float>;
+template class Vec2<int>;
--- src/Vec2.h
+++ src/Vec2.h
@@ -1,29 +1,31 @@
 #pragma once
+#define _USE_MATH_DEFINES
 
 #include "utils.h"
 #include <cmath>
 #include <print>
 #include <SFML/Graphics.hpp>
 
+template <typename T>
 class Vec2
 {
     public:
-        f32 x, y;
+        T x, y;
 
         Vec2() : x(0.0f), y(0.0f)
         {
         }
-        Vec2(f32 x, f32 y) : x(x), y(y)
+        Vec2(T x, T y) : x(x), y(y)
         {
         }
-        Vec2(float a) : x(a), y(a)
+        Vec2(T a) : x(a), y(a)
         {
         }
 
         bool operator==(const Vec2& rhs) const;
         Vec2 operator+(const Vec2& rhs) const;
         Vec2 operator-(const Vec2& rhs) const;
-        Vec2 operator*(const f32& scalar) const;
+        Vec2 operator*(const T& scalar) const;
         Vec2 operator/(const Vec2& rhs) const;
         void operator+=(const Vec2& rhs);
         void operator-=(const Vec2& rhs);
@@ -35,5 +37,6 @@ class Vec2
         void normalize();
         Vec2 normalized() const;
         static Vec2 randomPointInBounds(const Vec2& topLeft, const Vec2& bottomRight);
+        Vec2<f32>        rotate(f32 degrees) const;
         void print() const;
 };
