1e60d14dffe311d3211520af9cfbc025c178014e
Nathan Pedersen <ntpedersen1@gmail.com>
2025-08-10 23:09:52 -0800

    updated vec2 with templated version

--- src/CTransform.h
+++ src/CTransform.h
@@ -6,11 +6,11 @@
 class CTransform : public Component
 {
     public:
-        Vec2 pos      = {0, 0};
-        Vec2 velocity = {0, 0};
+        Vec2<f32> pos      = {0, 0};
+        Vec2<f32> velocity = {0, 0};
         f32  angle    = 0;
         CTransform()  = default;
-        CTransform(const Vec2& p, const Vec2& v, f32 a) : pos(p), velocity(v), angle(a)
+        CTransform(const Vec2<f32>& p, const Vec2<f32>& v, f32 a) : pos(p), velocity(v), angle(a)
         {
         }
 };
